bbd966ee7c4c3457448a60f2c9f14affe60ed280
brwngda <grz.barwinski@gmail.com>
2023-01-28 15:54:14 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PageMainComponent } from './page-main/page-main.component';
+import { PageProductFormComponent } from './page-product-form/page-product-form.component';
+import { PageProductListComponent } from './page-product-list/page-product-list.component';
+import {MainContainerComponent} from "./main-container/main-container.component";
+import {NavigatorComponent} from "./navigator/navigator.component";
+import {ContentContainerComponent} from "./content-container/content-container.component";
+import {AppRoutingModule} from "./app-routing.module";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainContainerComponent,
+    NavigatorComponent,
+    ContentContainerComponent,
+    PageMainComponent,
+    PageProductFormComponent,
+    PageProductListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
